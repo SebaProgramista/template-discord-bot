a02f2d0cf1bebaf5ff9d2b87a5d91e3fe25f199b
Sebastian <91561361+SebaProgramista@users.noreply.github.com>
2025-01-08 01:30:05 +0100

    Add new logger level

--- utils/logger_manager.py
+++ utils/logger_manager.py
@@ -2,6 +2,19 @@ import logging
 import colorlog
 import json
 
+class MessageLogger(logging.Logger):
+    MESSAGE_LEVEL = 25
+    
+    def __init__(self, name):
+        super().__init__(name)
+        logging.addLevelName(self.MESSAGE_LEVEL, "MESSAGE")
+        
+    def message(self, msg, *args, **kwargs):
+        if self.isEnabledFor(self.MESSAGE_LEVEL):
+            self._log(self.MESSAGE_LEVEL, msg, args, **kwargs)
+        
+logging.setLoggerClass(MessageLogger)
+
 class LoggerManager:
     def __init__(self, debug_mode):
         logger = logging.getLogger(__name__)
@@ -10,14 +23,15 @@ class LoggerManager:
         
         # Use colorlog's formatter
         formatter = colorlog.ColoredFormatter(
-            "%(fg_bold_black)s%(asctime)s %(levelname_log_color)s%(levelname)-8.5s%(reset)s %(purple)s%(funcName)s %(reset)s%(message)s",
+            "%(fg_bold_black)s%(asctime)s %(levelname_log_color)s%(levelname)-8.8s%(reset)s %(purple)s%(funcName)s %(reset)s%(message)s",
             datefmt="%Y-%m-%d %H:%M:%S",
             secondary_log_colors={
                 'levelname': {
                     'ERROR': 'red',
                     'INFO': 'blue',
                     'DEBUG': 'green',
-                    'WARNING': 'yellow'
+                    'WARNING': 'yellow',
+                    'MESSAGE': 'cyan'
                 }
             }
         )
